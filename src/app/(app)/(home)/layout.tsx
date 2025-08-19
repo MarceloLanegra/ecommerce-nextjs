d09dadea96067be3c4942f34644e2bc7891c925d
Marcelo <marcelo3096@gmail.com>
2025-08-19 11:35:29 -0500

    feat: added search filters

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,14 +1,43 @@
+import configPromise from '@payload-config'
+import { getPayload } from "payload"
 import Footer from "./footer"
 import Navbar from "./navbar"
+import SearchFilters from './search-filters'
+import { Category } from '@/payload-types'
 
 interface LayoutProps {
   children: React.ReactNode
 }
 
-function Layout({ children }: LayoutProps) {
+async function Layout({ children }: LayoutProps) {
+  const payload =  await getPayload({
+    config: configPromise
+  })
+
+  const data = await payload.find({
+    collection: 'categories',
+    pagination: false,
+    depth: 1,
+    where: {
+      parent: {
+        exists: false
+      }
+    }
+  })
+
+  const formattedData = data.docs.map((doc) =>({
+    ...doc,
+    subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
+      // Because of 'depth: 1' we need to cast the doc as Category
+      ...doc as Category,
+      subcategories: undefined
+    }))
+  }))
+
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
+      <SearchFilters data={formattedData} />
       <div className="flex-1 bg-[#F4F4F0]">
         {children}
       </div>
